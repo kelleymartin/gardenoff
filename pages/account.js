ba8fa85f7e0ac49603fd4d450472ba5f6215bf96
Kelley Martin <kelleymartin29@gmail.com>
2021-02-25 16:08:47 -0800

    base form

---
+++ pages/account.js
@@ -0,0 +1,210 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1>ACNH The Oasis Breeding Contest</h1>
+        <div className="container">
+          <div className="section">
+            <h3>Set up:</h3>
+            <div className="plot">
+              <div className="square"></div>
+              <div className="square"></div>
+              <div className="square"></div>
+              <div className="square"></div>
+              <div className="square"></div>
+              <div className="square"></div>
+              <div className="square"></div>
+              <div className="square"></div>
+              <div className="square"></div>
+              <div className="square"></div>
+              <div className="square"></div>
+              <div className="square"></div>
+              <div className="square"></div>
+              <div className="square"></div>
+              <div className="square"></div>
+              <div className="square"></div>
+              <div className="square"></div>
+              <div className="square"></div>
+              <div className="square"></div>
+              <div className="square"></div>
+              <div className="square"></div>
+              <div className="square"></div>
+              <div className="square"></div>
+              <div className="square"></div>
+              <div className="square"></div>
+            </div>
+          </div>
+          <div className="section">
+            <h3>Visitors:</h3>
+            <div className="polaroid">
+              <img
+                className="group"
+                src="https://cdn.discordapp.com/attachments/727247737191530578/804407351489855548/2021012712574100-02CB906EA538A35643C1E1484C4B947D.jpg"
+              />
+              <div className="tags">
+                <div className="tag">
+                  <svg
+                    className="eye"
+                    width="50"
+                    height="50"
+                    viewBox="0 0 50 50"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M25 0C28.0016 -4.58838e-07 30.1764 5.68148 30.1764 5.68148C30.1764 5.68148 34.9006 1.84859 37.5 3.34936C40.0994 4.85014 39.1421 10.8579 39.1421 10.8579C39.1421 10.8579 45.1499 9.90057 46.6506 12.5C48.1514 15.0994 44.3185 19.8236 44.3185 19.8236C44.3185 19.8236 50 21.9984 50 25C50 28.0016 44.3185 30.1764 44.3185 30.1764C44.3185 30.1764 48.1514 34.9006 46.6506 37.5C45.1499 40.0994 39.1421 39.1421 39.1421 39.1421C39.1421 39.1421 40.0994 45.1499 37.5 46.6506C34.9006 48.1514 30.1764 44.3185 30.1764 44.3185C30.1764 44.3185 28.0016 50 25 50C21.9984 50 19.8236 44.3185 19.8236 44.3185C19.8236 44.3185 15.0994 48.1514 12.5 46.6506C9.90057 45.1499 10.8579 39.1421 10.8579 39.1421C10.8579 39.1421 4.85014 40.0994 3.34936 37.5C1.84859 34.9006 5.68148 30.1764 5.68148 30.1764C5.68148 30.1764 -4.58838e-07 28.0016 0 25C4.58838e-07 21.9984 5.68148 19.8236 5.68148 19.8236C5.68148 19.8236 1.84859 15.0994 3.34936 12.5C4.85014 9.90057 10.8579 10.8579 10.8579 10.8579C10.8579 10.8579 9.90057 4.85014 12.5 3.34936C15.0994 1.84859 19.8236 5.68148 19.8236 5.68148C19.8236 5.68148 21.9984 4.58838e-07 25 0Z"
+                      fill="#B3B3B3"
+                    />
+                    <circle cx="25" cy="25" r="15" fill="white" />
+                    <circle cx="20" cy="25" r="10" fill="#5E5E5E" />
+                  </svg>
+                  EmmiKay
+                </div>
+                <div className="tag">
+                  <svg
+                    className="eye"
+                    width="50"
+                    height="50"
+                    viewBox="0 0 50 50"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M25 0C28.0016 -4.58838e-07 30.1764 5.68148 30.1764 5.68148C30.1764 5.68148 34.9006 1.84859 37.5 3.34936C40.0994 4.85014 39.1421 10.8579 39.1421 10.8579C39.1421 10.8579 45.1499 9.90057 46.6506 12.5C48.1514 15.0994 44.3185 19.8236 44.3185 19.8236C44.3185 19.8236 50 21.9984 50 25C50 28.0016 44.3185 30.1764 44.3185 30.1764C44.3185 30.1764 48.1514 34.9006 46.6506 37.5C45.1499 40.0994 39.1421 39.1421 39.1421 39.1421C39.1421 39.1421 40.0994 45.1499 37.5 46.6506C34.9006 48.1514 30.1764 44.3185 30.1764 44.3185C30.1764 44.3185 28.0016 50 25 50C21.9984 50 19.8236 44.3185 19.8236 44.3185C19.8236 44.3185 15.0994 48.1514 12.5 46.6506C9.90057 45.1499 10.8579 39.1421 10.8579 39.1421C10.8579 39.1421 4.85014 40.0994 3.34936 37.5C1.84859 34.9006 5.68148 30.1764 5.68148 30.1764C5.68148 30.1764 -4.58838e-07 28.0016 0 25C4.58838e-07 21.9984 5.68148 19.8236 5.68148 19.8236C5.68148 19.8236 1.84859 15.0994 3.34936 12.5C4.85014 9.90057 10.8579 10.8579 10.8579 10.8579C10.8579 10.8579 9.90057 4.85014 12.5 3.34936C15.0994 1.84859 19.8236 5.68148 19.8236 5.68148C19.8236 5.68148 21.9984 4.58838e-07 25 0Z"
+                      fill="#FFA5E6"
+                    />
+                    <circle cx="25" cy="25" r="15" fill="white" />
+                    <path
+                      d="M30 14C26.3333 14 25 18 25 18C25 18 23.6667 14 20 14C16.3333 14 14 18 14 22C14 28 25 36 25 36C25 36 36 28 36 22C36 18 33.6667 14 30 14Z"
+                      fill="#D44983"
+                    />
+                  </svg>
+                  Stas
+                </div>
+              </div>
+            </div>
+            <button className="download">Download</button>
+          </div>
+          <div className="section">
+            <h3>Results:</h3>
+            <div className="plot">
+              <div className="square"></div>
+              <div className="square"></div>
+              <div className="square"></div>
+              <div className="square"></div>
+              <div className="square"></div>
+              <div className="square"></div>
+              <div className="square"></div>
+              <div className="square"></div>
+              <div className="square"></div>
+              <div className="square"></div>
+              <div className="square"></div>
+              <div className="square"></div>
+              <div className="square"></div>
+              <div className="square"></div>
+              <div className="square"></div>
+              <div className="square"></div>
+              <div className="square"></div>
+              <div className="square"></div>
+              <div className="square"></div>
+              <div className="square"></div>
+              <div className="square"></div>
+              <div className="square"></div>
+              <div className="square"></div>
+              <div className="square"></div>
+              <div className="square"></div>
+            </div>
+          </div>
+        </div>
+        <div className="container">
+          <div className="calendar">
+            <div className="day">
+              <p className="numberDay">Day 1</p>
+              <p className="date">March 3rd</p>
+              <ul>
+                <li>Set up:</li>
+                <li>5 waterers</li>
+                <li>Blooms</li>
+                <li>New flowers</li>
+              </ul>
+            </div>
+            <div className="day">
+              <p className="numberDay">Day 2</p>
+              <p className="date">March 4th</p>
+              <ul>
+                <li>Set up:</li>
+                <li>5 waterers</li>
+                <li>Blooms</li>
+                <li>New flowers</li>
+              </ul>
+            </div>
+            <div className="day">
+              <p className="numberDay">Day 3</p>
+              <p className="date">March 5th</p>
+              <ul>
+                <li>Set up:</li>
+                <li>5 waterers</li>
+                <li>Blooms</li>
+                <li>New flowers</li>
+              </ul>
+            </div>
+            <div className="day">
+              <button className="newDay">Add a day!</button>
+            </div>
+          </div>
+        </div>
+        <div className="container inputSection">
+          <div>
+            <label>Day #</label>
+            <input className="dayNumberInput" type="number" required></input>*
+          </div>
+          <div>
+            <label>Date:</label>
+            <input
+              type="date"
+              min="2021-03-01"
+              max="2021-03-31"
+              required
+            ></input>
+            *
+          </div>
+          <div>
+            <label>Set up:</label>
+            <input className="imageUpload" type="file"></input>
+            or
+            <input
+              className="textField"
+              type="text"
+              placeholder="Write about your setup"
+            ></input>
+          </div>
+          <div>
+            <label>Waterers:</label>
+            <input className="imageUpload" type="file"></input>
+            or
+            <input type="search" placeholder="Add waterer"></input>
+          </div>
+          <div>
+            <label>Results:</label>
+            <input className="imageUpload" type="file"></input>
+            or
+            <input type="checkbox"></input>Orange cosmo
+            <input type="checkbox"></input>Pink cosmo
+            <input type="checkbox"></input>Black cosmo
+            <input type="checkbox"></input>Orange lily
+            <input type="checkbox"></input>Pink lily
+            <input type="checkbox"></input>Black lily
+          </div>
+          <input className="submit" type="submit"></input>
+        </div>
+      </main>
+    </div>
+  );
+}
